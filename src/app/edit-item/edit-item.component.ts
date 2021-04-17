3d56d3fd63227d0fb45b4ef35423c1489b0ae5df
jacobRinehart42 <jacobofmirkwood@yahoo.com>
2021-04-17 16:18:54 -0500

    2 files modified

--- src/app/edit-item/edit-item.component.ts
+++ src/app/edit-item/edit-item.component.ts
@@ -1,28 +1,47 @@
-import { Component, OnInit } from '@angular/core';
-import { RoutingService } from '../routing.service';
-import { ItemsService } from '../items.service';
+import { Component, OnInit } from "@angular/core";
+import { RoutingService } from "../routing.service";
+import { ItemsService } from "../items.service";
 
 @Component({
-  selector: 'app-edit-item',
-  templateUrl: './edit-item.component.html',
-  styleUrls: ['./edit-item.component.css']
+  selector: "app-edit-item",
+  templateUrl: "./edit-item.component.html",
+  styleUrls: ["./edit-item.component.css"]
 })
 export class EditItemComponent implements OnInit {
-
   currentItem;
 
-  constructor (private route: RoutingService, public its: ItemsService) {}
+  constructor(private route: RoutingService, public its: ItemsService) {}
 
   ngOnInit() {
     this.currentItem = this.its.selectedItem;
   }
 
-  save(): void {
-
+  save(
+    name,
+    askingPrice,
+    nicheMarket,
+    TimeRanges,
+    description,
+    dimensions,
+    conditionAndAge,
+    otherInfo
+  ): void {
+    this.its.editItem(
+      this.currentItem.itemId,
+      name,
+      askingPrice,
+      "seller",
+      nicheMarket,
+      TimeRanges,
+      description,
+      dimensions,
+      conditionAndAge,
+      otherInfo
+    );
+    this.switchToMyItems();
   }
 
   switchToMyItems() {
     this.route.onShowMyItems();
   }
-
 }
